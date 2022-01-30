eaf4217e2820bdd8ecd1f92f60b2a6e7341e1bb6
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-01-30 05:10:37 +0100

    WIP

--- src/main/java/com/example/flashcards/formatter/BasicTextFormatter.java
+++ src/main/java/com/example/flashcards/formatter/BasicTextFormatter.java
@@ -1,9 +1,11 @@
 package com.example.flashcards.formatter;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("originalText")
 public class BasicTextFormatter implements TextFormatter{
 
     @Override
--- src/main/java/com/example/flashcards/formatter/UpperCaseFormatter.java
+++ src/main/java/com/example/flashcards/formatter/UpperCaseFormatter.java
@@ -1,10 +1,11 @@
 package com.example.flashcards.formatter;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@Profile("upperCaseText")
 public class UpperCaseFormatter implements TextFormatter{
 
     @Override
